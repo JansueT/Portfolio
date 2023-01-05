ef72abb4e648fb3764e4c02b8f96ebb010f7ed60
JansueT <cansu8tas@gmail.com>
2023-01-05 13:24:14 +0300

    added linter files and node_modules

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-const menuIconElement = document.getElementById("menu_icon");
-const menuElement = document.getElementById("menu");
-const closeMenuElement = document.getElementById("close_menu_icon");
-const links = document.getElementsByClassName("nav_items");
-const logo = document.querySelector(".header_logo");
+const menuIconElement = document.getElementById('menu_icon');
+const menuElement = document.getElementById('menu');
+const closeMenuElement = document.getElementById('close_menu_icon');
+const links = document.getElementsByClassName('nav_items');
+const logo = document.querySelector('.header_logo');
 
-menuIconElement.addEventListener("click", () => {
-  menuIconElement.classList.add("hide_menu");
-  menuElement.classList.add("cansu_menu");
-  document.body.style.overflowY = "hidden";
-  closeMenuElement.classList.remove("hide_menu");
-  logo.classList.add("hide_menu");
+menuIconElement.addEventListener('click', () => {
+  menuIconElement.classList.add('hide_menu');
+  menuElement.classList.add('cansu_menu');
+  document.body.style.overflowY = 'hidden';
+  closeMenuElement.classList.remove('hide_menu');
+  logo.classList.add('hide_menu');
 });
 
-closeMenuElement.addEventListener("click", () => {
-  menuIconElement.classList.remove("hide_menu");
-  document.body.style.overflowY = "scroll";
-  closeMenuElement.classList.add("hide_menu");
-  menuElement.classList.remove("cansu_menu");
-  logo.classList.remove("hide_menu");
+closeMenuElement.addEventListener('click', () => {
+  menuIconElement.classList.remove('hide_menu');
+  document.body.style.overflowY = 'scroll';
+  closeMenuElement.classList.add('hide_menu');
+  menuElement.classList.remove('cansu_menu');
+  logo.classList.remove('hide_menu');
 });
 
 for (let i = 0; i < links.length; i += 1) {
-  links[i].addEventListener("click", () => {
-    document.body.style.overflowY = "scroll";
-    menuIconElement.classList.remove("hide_menu");
-    menuElement.classList.remove("cansu_menu");
-    closeMenuElement.classList.add("hide_menu");
-    logo.classList.remove("hide_menu");
+  links[i].addEventListener('click', () => {
+    document.body.style.overflowY = 'scroll';
+    menuIconElement.classList.remove('hide_menu');
+    menuElement.classList.remove('cansu_menu');
+    closeMenuElement.classList.add('hide_menu');
+    logo.classList.remove('hide_menu');
   });
 }
