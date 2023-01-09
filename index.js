f38ebba66fec7c9b010f910e4d89b8755a37b9e8
JansueT <cansu8tas@gmail.com>
2023-01-09 16:09:43 +0300

    modified index.js

--- index.js
+++ index.js
@@ -37,7 +37,6 @@ const formElement = document.getElementById('form_id');
 formElement.addEventListener('submit', (event) => {
   if (emailElement.value.toLowerCase() !== emailElement.value) {
     event.preventDefault();
-    smallElement.innerText =
-      'please type all email charaters in lowercase. form was not submitted';
+    smallElement.innerText = 'please type all email charaters in lowercase. form was not submitted';
   }
 });
