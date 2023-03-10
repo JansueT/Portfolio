e93be2dea60a2bca680e14df4c9ae609c1a44da8
JansueT <cansu8tas@gmail.com>
2023-03-10 16:28:36 +0300

    added js

--- index.js
+++ index.js
@@ -40,3 +40,41 @@ formElement.addEventListener('submit', (event) => {
     smallElement.innerText = 'please type all email charaters in lowercase. form was not submitted';
   }
 });
+
+const formInput = document.getElementById('form_id');
+
+const store = (key, value) => {
+  localStorage.setItem(key, value);
+};
+
+const saveForm = (event) => {
+  const data = {};
+  event.preventDefault();
+  if (formInput.elements.name.value) {
+    data.name = formInput.elements.name.value;
+  }
+  if (formInput.elements.email.value) {
+    data.email = formInput.elements.email.value;
+  }
+  if (formInput.elements.text.value) {
+    data.text = formInput.elements.text.value;
+  }
+  store('data', JSON.stringify(data));
+  window.localStorage.setItem('formdata', data);
+};
+formInput.elements.name.addEventListener('input', saveForm);
+formInput.elements.email.addEventListener('input', saveForm);
+formInput.elements.text.addEventListener('input', saveForm);
+
+const showSavedData = () => {
+  if (localStorage.getItem('data')) {
+    const userData = JSON.parse(localStorage.getItem('data'));
+    formInput.elements.name.value = userData.name;
+    formInput.elements.email.value = userData.email;
+    formInput.elements.text.value = userData.text;
+  }
+};
+
+showSavedData();
+
+formInput.addEventListener('submit', saveForm);
