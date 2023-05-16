75ee49a22245ebe50c37568aa12ad87b202d5266
fares aljuaifari <fares.aljuaifri@gmail.com>
2023-05-16 11:38:42 +0300

    added code to js file and fix the linters  node-version

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
-const menuIconElement = document.getElementById('menu_icon');
-const menuElement = document.getElementById('menu');
-const closeMenuElement = document.getElementById('close_menu_icon');
-const links = document.getElementsByClassName('nav_items');
-const logo = document.querySelector('.header_logo');
+const menuIconElement = document.getElementById("menu_icon");
+const menuElement = document.getElementById("menu");
+const closeMenuElement = document.getElementById("close_menu_icon");
+const links = document.getElementsByClassName("nav_items");
+const logo = document.querySelector(".header_logo");
 
-menuIconElement.addEventListener('click', () => {
-  menuIconElement.classList.add('hide_menu');
-  menuElement.classList.add('cansu_menu');
-  document.body.style.overflowY = 'hidden';
-  closeMenuElement.classList.remove('hide_menu');
-  logo.classList.add('hide_menu');
+menuIconElement.addEventListener("click", () => {
+  menuIconElement.classList.add("hide_menu");
+  menuElement.classList.add("cansu_menu");
+  document.body.style.overflowY = "hidden";
+  closeMenuElement.classList.remove("hide_menu");
+  logo.classList.add("hide_menu");
 });
 
-closeMenuElement.addEventListener('click', () => {
-  menuIconElement.classList.remove('hide_menu');
-  document.body.style.overflowY = 'scroll';
-  closeMenuElement.classList.add('hide_menu');
-  menuElement.classList.remove('cansu_menu');
-  logo.classList.remove('hide_menu');
+closeMenuElement.addEventListener("click", () => {
+  menuIconElement.classList.remove("hide_menu");
+  document.body.style.overflowY = "scroll";
+  closeMenuElement.classList.add("hide_menu");
+  menuElement.classList.remove("cansu_menu");
+  logo.classList.remove("hide_menu");
 });
 
-// for (let i = 0; i < links.length; i += 1) {
-//   links[i].addEventListener('click', () => {
-//     document.body.style.overflowY = 'scroll';
-//     menuIconElement.classList.remove('hide_menu');
-//     menuElement.classList.remove('cansu_menu');
-//     closeMenuElement.classList.add('hide_menu');
-//     logo.classList.remove('hide_menu');
-//   });
-// }
+for (let i = 0; i < links.length; i += 1) {
+  links[i].addEventListener("click", () => {
+    document.body.style.overflowY = "scroll";
+    menuIconElement.classList.remove("hide_menu");
+    menuElement.classList.remove("cansu_menu");
+    closeMenuElement.classList.add("hide_menu");
+    logo.classList.remove("hide_menu");
+  });
+}
 
-const smallElement = document.getElementById('error');
-const emailElement = document.getElementById('email_id');
-const formElement = document.getElementById('form_id');
+const smallElement = document.getElementById("error");
+const emailElement = document.getElementById("email_id");
+const formElement = document.getElementById("form_id");
 
-formElement.addEventListener('submit', (event) => {
+formElement.addEventListener("submit", (event) => {
   if (emailElement.value.toLowerCase() !== emailElement.value) {
     event.preventDefault();
-    smallElement.innerText = 'please type all email charaters in lowercase. form was not submitted';
+    smallElement.innerText =
+      "please type all email charaters in lowercase. form was not submitted";
   }
 });
